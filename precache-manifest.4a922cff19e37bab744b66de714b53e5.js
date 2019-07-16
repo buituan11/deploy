e2a2c4e39eb4bff989ac81858f3be27432f5738a
buituan11 <buituan71099@gmail.com>
2019-07-17 00:12:28 +0700

    Updates

---
+++ precache-manifest.4a922cff19e37bab744b66de714b53e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc358805daec20e8a8fd49ee4976fa3",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "83e5674bbdc91e7b7061",
+    "url": "/deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "83e5674bbdc91e7b7061",
+    "url": "/deploy/static/js/main.dafb7322.chunk.js"
+  },
+  {
+    "revision": "0dcb4eec97520abc4f58",
+    "url": "/deploy/static/js/runtime~main.9c98c2a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
